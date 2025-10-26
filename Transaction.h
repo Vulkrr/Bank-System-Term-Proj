6139c32288d9e8bf52035172cd29226ee96e9671
vulkrr <vulklifts@gmail.com>
2025-10-26 15:19:54 -0400

    (WIP) Added Transaction class. Inherits from both SavingAccount and CheckingAccount.

--- Transaction.h
+++ Transaction.h
@@ -1,29 +1,25 @@
 #pragma once
 #ifndef _TRANSACTION_
 #define _TRANSACTION_
-#include "Account.h"
+#include "SavingAccount.h"
+#include "CheckingAccount.h"
 
-class Account {
-	int ID, withdrawCount, depositCount;
-	double bal;
-	Customer * accCustomer;
+class Transaction : public SavingAccount, public CheckingAccount {
+	string transactionType;
+	double transactionAmount, snapshotBalance;
+	bool overdraft;
 public:
-	Account();
-	Account(string _fname, string _lname, string _address, string _email, string _phone, int _ID, int _withdrawCount, int _depositCount, double _bal, Customer* _accCustomer);
+	Transaction()
+	{
+		transactionType = "";
+		transactionAmount = 0;
+		snapshotBalance = 0;
+		overdraft = false;
+	}
 
-	void setAll(string _fname, string _lname, string _address, string _email, string _phone, int _ID, int _withdrawCount, int _depositCount, double _bal, Customer* _accountCustomer);
-	void setID(int _ID);
-	void setWithdrawCount(int _withdrawCount);
-	void setDepositCount(int _depositCount);
-	void setBal(double _bal);
+	Transaction(string _transactionType, double transactionAmount, double snapshotBalance, bool overdraft)
+	{
 
-	int getID() const;
-	int getWithdrawCount() const;
-	int getDepositCount() const;
-	double getBal() const;
-
-	void deposit(double _amount);
-	void withdraw(double _amount);
-	void printInfo();
+	}
 };
 #endif // !_ACCOUNT_
