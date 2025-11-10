d31a709b05e564b9b7142c34b8c5a9fb5eab0c43
ChristianLeslie <not5normal@gmail.com>
2025-11-10 11:16:02 -0500

    Create Index.h

---
+++ Index.h
@@ -0,0 +1,13 @@
+#pragma once
+#ifndef _INDEX_
+#define _INDEX_
+using namespace std;
+
+int totalCustomerCount = 0, customerIndex = 0,
+checkingAccCount = 0, checkingAccIndex = 0,
+savingAccCount = 0, savingAccIndex = 0,
+transferPullIndex = 0, transferPushIndex = 0,
+transactionCount = 0;
+
+
+#endif _INDEX_
