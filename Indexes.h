6ecba773bf5a269874c7d3bc2ad186e1cedbdf66
ChristianLeslie <not5normal@gmail.com>
2025-11-14 12:58:22 -0500

    Update Indexes.h

--- Indexes.h
+++ Indexes.h
@@ -1,12 +1,1 @@
-#pragma once
-#ifndef _INDEXES_
-#define _INDEXES_
-using namespace std;
 
-int totalCustomerCount = 0, customerIndex = 0,
-checkingAccCount = 0, checkingAccIndex = 0,
-savingAccCount = 0, savingAccIndex = 0,
-transferPullIndex = 0, transferPushIndex = 0,
-transactionCount = 0, generalAccIndex = 0;
-
-#endif _INDEXES_
